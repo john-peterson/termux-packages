8d9b4e4dec0921c3426e8fe7ca7d8fd21fe1baf6
John Sebastian Peterson <jpeterson57@gmail.com>
2025-03-18 11:02:52 +1100

    clean

--- scripts/build/get_source/termux_git_clone_src.sh
+++ scripts/build/get_source/termux_git_clone_src.sh
@@ -38,9 +38,6 @@ termux_git_clone_src() {
 		popd
 
 		echo "$TERMUX_PKG_VERSION" > $TMP_CHECKOUT_VERSION
-	fi
-
-	if ! $TERMUX_NO_CLEAN_SRC; then
 	rm -rf $TERMUX_PKG_SRCDIR
 	cp -Rf $TMP_CHECKOUT $TERMUX_PKG_SRCDIR
 	fi
