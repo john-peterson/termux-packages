93c2fa29a834ede7939a4ac28808eeb7c924e86e
John Sebastian Peterson <jpeterson57@gmail.com>
2025-11-12 20:25:16 +1100

    tentative  cargo safe build  . cargo patch needed to separate root and install path
    I'm looking for the cargo equivalent of
    
    	configure --prefix=abc
    	make install DESTDIR=123
    
    this command should not install $PREFIX/bin/test
    
    	bash build-package.sh -s -f test --safe --fast
    	$PREFIX/bin/test
    
    this is the best solution I could find to change the prefix essentially . apps will break if they try to use this path . it's only for staging the install not the actual install path
    
    I haven't found any actual app that use the root flag . this has to be solved when when the problem appears . when some one needs a safe cargo build
    
     # test
    
    cargo doesn't separate prefix and install path the same way as auto tools . this could be patched in future cargo versions
    
    I  found this flag but it's not correct it still install to abc
    
    	CARGO_INSTALL_ROOT=123 cargo install --path . --root abc
    
    I tried these with no effect
    
    	CARGO_DESTDIR=123
    	CARGO_DEST_DIR=123
    	CARGO_INSTALL_DIR=123

--- scripts/build/termux_step_make_install.sh
+++ scripts/build/termux_step_make_install.sh
@@ -1,7 +1,6 @@
 # shellcheck disable=SC2086
 termux_step_make_install() {
 	[ "$TERMUX_PKG_METAPACKAGE" = "true" ] && return
-
 	if test -f build.ninja; then
 		ninja -j $TERMUX_PKG_MAKE_PROCESSES $TERMUX_PKG_MAKE_INSTALL_TARGET 
 	elif test -f setup.py || test -f pyproject.toml || test -f setup.cfg; then
@@ -22,6 +21,8 @@ termux_step_make_install() {
 		fi
 	elif test -f Cargo.toml; then
 		termux_setup_rust
+		# this has no effect 
+		# CARGO_TARGET_DIR=$TERMUX_PKG_CARGO_INSTALL_TARGET \
 		cargo install \
 			--jobs $TERMUX_PKG_MAKE_PROCESSES \
 			--path . \
@@ -29,7 +30,7 @@ termux_step_make_install() {
 			--locked \
 			--no-track \
 			--target $CARGO_TARGET_NAME \
-			--root $TERMUX_PREFIX_BASE \
+			--root $TERMUX_PKG_CARGO_INSTALL_TARGET \
 			$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 	fi
 }
